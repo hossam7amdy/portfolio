7b7723f5748c3b388683843e07e804cb939eb8de
Hossam Hamdy <hossamhamdy117@gmail.com>
2024-08-05 20:24:53 +0300

    chore: Update image rendering in PortfolioItem and Header components

--- src/app/components/PortfolioItem/PortfolioItem.jsx
+++ src/app/components/PortfolioItem/PortfolioItem.jsx
@@ -3,6 +3,7 @@ import { AiFillGithub } from "react-icons/ai";
 
 import { IconLink } from "../UI/IconLink/IconLink";
 import styles from "./PortfolioItem.module.css";
+import Image from "next/image";
 
 export const PortfolioItem = ({
   index,
@@ -18,7 +19,13 @@ export const PortfolioItem = ({
       className={styles.project}
       style={{ transform: `translateX(${index * -100}%)` }}
     >
-      <img className={styles.img} src={img} alt={name} />
+      <Image
+        className={styles.img}
+        src={img}
+        alt={name}
+        width={650}
+        height={350}
+      />
       <div className={styles.content}>
         <h3 className={styles.title}>{name}</h3>
         <p className={styles.decription}>{decription}</p>
--- src/app/container/Header/Header.jsx
+++ src/app/container/Header/Header.jsx
@@ -6,6 +6,7 @@ import { AiOutlineAppstore } from "react-icons/ai";
 import styles from "./Header.module.css";
 import { Navbar } from "../../components/Navbar/Navbar";
 import { ColorModeSwitcher } from "../../components/ColorModeSwitcher/ColorModeSwitcher";
+import Image from "next/image";
 
 export const Header = () => {
   const [show, setShow] = useState(false);
@@ -13,7 +14,7 @@ export const Header = () => {
   return (
     <header className={styles.header}>
       <a href="#home" className={styles.logo}>
-        <img src="/logo.png" alt="logo" />
+        <Image priority src="/logo.png" alt="logo" width={32} height={32} />
       </a>
       <nav className={styles.nav}>
         <Navbar show={show} onClose={() => setShow(false)} />
