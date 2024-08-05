a807181cd9e27d0617eb691e1a356a6991d0a4e5
Hossam Hamdy <38383270+hossam7amdy@users.noreply.github.com>
2024-08-05 20:26:10 +0300

    Nextjs (#14)
    * initial migration to nextjs app router 🚀
    
    * chore: Update deployment workflow to target "nextjs" branch
    
    * chore: Update deployment workflow to target "out" folder
    
    * Remove unused SVG files
    
    * chore: Remove Next.js Image component and use regular img tag for images
    
    * chore: Update theme toggle logic and localStorage handling
    
    * chore: Update Navbar component to use Next.js Link for navigation
    
    * chore: Add nodemailer dependency for sending emails
    
    * chore: Update image rendering in PortfolioItem and Header components

---
+++ src/app/actions/index.js
@@ -0,0 +1,56 @@
+"use server";
+
+import { z } from "zod";
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  secure: true,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
+
+const FormSchema = z.object({
+  name: z.string().min(3).max(50),
+  project: z.string().min(3).max(50),
+  email: z.string().email(),
+  message: z.string().min(10).max(500),
+});
+
+export const sendEmail = async (_prevState, formData) => {
+  const validatedFields = FormSchema.safeParse({
+    name: formData.get("name"),
+    email: formData.get("email"),
+    project: formData.get("project"),
+    message: formData.get("message"),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to send an email.",
+    };
+  }
+
+  const { name, email, project, message } = validatedFields.data;
+
+  const mailOptions = {
+    from: email,
+    to: process.env.MAIL_RECIPIENT,
+    subject: `New message from ${name} about ${project}`,
+    text: message,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    return {
+      message: null,
+    };
+  } catch (error) {
+    return {
+      message: "Failed to send an email.",
+    };
+  }
+};
